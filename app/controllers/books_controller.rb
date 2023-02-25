241a3c422ef581629504a18f1670f8a95c357156
ameamefurefure <124484020+ameamefurefure@users.noreply.github.com>
2023-02-25 09:48:20 +0000

    バリデーション実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,11 @@
 class BooksController < ApplicationController
   def new
-    @book = Book.new  
+    @book = Book.new
   end
-  
+
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -14,32 +15,42 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
+
+  # def create
+  #   # １.&2. データを受け取り新規登録するためのインスタンス作成
+  #   book = Book.new(book_params)
+  #   # 3. データをデータベースに保存するためのsaveメソッド実行
+  #   book.save
+  #   # 4. 詳細画面へリダイレクト
+  #   flash[:notice] = "Book was successfully created"
+  #   redirect_to book_path(book.id)
+  # end
+
   def create
-    # １.&2. データを受け取り新規登録するためのインスタンス作成
-    book = Book.new(book_params)
-    # 3. データをデータベースに保存するためのsaveメソッド実行
-    book.save
-    # 4. 詳細画面へリダイレクト
-    flash[:notice] = "Book was successfully created"
-    redirect_to book_path(book.id)
-    
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created"
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
-  
-  
+
+
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to book_path(book.id)  
+    redirect_to book_path(book.id)
   end
-  
+
   def destroy
     book = Book.find(params[:id])  # データ（レコード）を1件取得
     book.destroy  # データ（レコード）を削除
-    redirect_to '/books'  # 投稿一覧画面へリダイレクト  
+    redirect_to '/books'  # 投稿一覧画面へリダイレクト
   end
-  
-  
+
+
   private
   # ストロングパラメータ
   def book_params
